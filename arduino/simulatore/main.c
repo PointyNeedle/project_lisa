f1fb284ae348eddd5e9c58b4d89bf8d14d38c166
Agostino Saviano <agostinosaviano01@gmail.com>
2018-05-30 21:42:23 +0200

    Scritto primo sketch simulatore dispositivo

---
+++ arduino/simulatore/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+void init();
+void controlla_movimento();
+void controlla_autenticazione();
+void ottieni_coordinate();
+
+bool loggato, in_movimento;
+float posizioni[2]; // [0] = latitudine, [1] = longitudine
+
+int main(void)
+  {
+    init();
+
+    // uscita dal ciclo di polling solo in caso di situazione anomala
+    // (auto in movimento mentre l'utente non è loggato)
+    while (!in_movimento && loggato)
+    {
+      controlla_autenticazione();
+      controlla_movimento();
+    }
+
+    // lettura coordinate gps dal sensore
+    ottieni_coordinate();
+
+    // stabilisci connessione
+
+    // manda pacchetto
+    return 0;
+  }
+
+void init()
+  {
+    loggato = false;
+    in_movimento = false;
+  }
+
