89b58fa1ad17a6d97d597014ca8960f48b20bec0
Agostino Saviano <agostinosaviano01@gmail.com>
2018-06-05 16:15:50 +0200

    Aggiunti test per i sensori

---
+++ arduino/tests/RFID_test.ino
@@ -0,0 +1,50 @@
+#include <EEPROM.h> // We are going to read and write PICC's UIDs from/to EEPROM
+#include <SPI.h>    // RC522 Module uses SPI protocol
+#include <MFRC522.h>
+
+#define RST_PIN 9 // Configurable, see typical pin layout above
+#define SS_PIN 10 // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+byte readCard[4];                 // Stores scanned ID read from RFID Module
+byte card[4] = {0x6F, 0x38, 0xF9, 0x28};
+
+void setup()
+{
+  Serial.begin(9600);                // Initialize serial communications with the PC
+  while (!Serial);                   // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();                       // Init SPI bus
+  mfrc522.PCD_Init();                // Init MFRC522
+}
+
+void loop()
+{
+  getID();
+  if (readCard[0] == card[0])
+    Serial.println("KEK");
+}
+
+uint8_t getID()
+{
+  // Getting ready for Reading PICCs
+  if (!mfrc522.PICC_IsNewCardPresent())
+  { //If a new PICC placed to RFID reader continue
+    return 0;
+  }
+  if (!mfrc522.PICC_ReadCardSerial())
+  { //Since a PICC placed get Serial and continue
+    return 0;
+  }
+  // There are Mifare PICCs which have 4 byte or 7 byte UID care if you use 7 byte PICC
+  // I think we should assume every PICC as they have 4 byte UID
+  // Until we support 7 byte PICCs
+  Serial.println(F("Scanned PICC's UID:"));
+  for (uint8_t i = 0; i < 4; i++)
+  { //
+    readCard[i] = mfrc522.uid.uidByte[i];
+    Serial.print(readCard[i], HEX);
+  }
+  Serial.println("");
+  mfrc522.PICC_HaltA(); // Stop reading
+  return 1;
+}
